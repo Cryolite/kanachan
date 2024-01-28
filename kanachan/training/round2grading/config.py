b3343d5e9e52b554c2fdfb374e2b8427915816d0
Cryolite <cryolite.indigo@gmail.com>
2024-01-28 00:30:14 +0000

    Add `kanachan.training.round2grading`

---
+++ kanachan/training/round2grading/config.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+from dataclasses import dataclass, field
+from typing import Any
+from omegaconf import MISSING
+from hydra.core.config_store import ConfigStore
+import kanachan.training.core.config.device # pylint: disable=unused-import
+import kanachan.training.core.config.encoder # pylint: disable=unused-import
+import kanachan.training.core.config.optimizer # pylint: disable=unused-import
+
+
+_defaults = [
+    { 'device': 'cuda' },
+    { 'encoder': 'bert_base' },
+    { 'decoder': 'single' },
+    { 'optimizer': 'adam' },
+    '_self_'
+]
+
+
+@dataclass
+class Config:
+    defaults: list[Any] = field(default_factory=lambda: _defaults)
+    training_data: Path = MISSING
+    num_workers: int | None = None
+    celestial_scale: int = 450
+    initial_model_prefix: Path | None = None
+    initial_model_index: int | None = None
+    checkpointing: bool = False
+    batch_size: int = MISSING
+    loss_function: str = 'symlog'
+    gradient_accumulation_steps: int = 1
+    max_gradient_norm: float = 1.0
+    snapshot_interval: int = 0
+
+
+config_store = ConfigStore.instance()
+config_store.store(name='config', node=Config)
