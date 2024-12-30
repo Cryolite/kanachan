fa8563f16f25ce0e595820d3797f95536f170d8b
Cryolite <cryolite.indigo@gmail.com>
2024-12-30 01:14:06 +0000

    Tweak

--- kanachan/nn/__init__.py
+++ kanachan/nn/__init__.py
@@ -1,12 +1,10 @@
-from .noisy_linear import NoisyLinear
-from .positional_encoding import PositionalEncoding
-from .position_embedding import PositionEmbedding
-from .encoder import Encoder
-from .round_encoder import RoundEncoder
-from .decoder import Decoder
-from .qr_decoder import QRDecoder, QDecoder
 from .decode_converter import DecodeConverter
-from .twin_q_actor import TwinQActor
+from .decoder import Decoder
+from .encoder import Encoder
 from .epsilon_greedy import EpsilonGreedy
-from .symlog_loss import SymlogLoss
+from .noisy_linear import NoisyLinear
+from .qr_decoder import QDecoder, QRDecoder
+from .round_encoder import RoundEncoder
 from .symexp import Symexp
+from .symlog_loss import SymlogLoss
+from .twin_q_actor import TwinQActor
